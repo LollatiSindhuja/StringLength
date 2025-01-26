a9fdb90bda2ae1181183e95a50fa9c5365df705f
LollatiSindhuja <sindhujalollati@gmail.com>
2025-01-26 18:45:24 +0530

    Add files via upload

---
+++ LengthOfString.java
@@ -0,0 +1,8 @@
+public class LengthOfString {
+    public static void main(String[] args) {
+        String str="Hello world";
+        int length=str.length();
+        System.out.println("length:"+length);
+    }
+    
+}
